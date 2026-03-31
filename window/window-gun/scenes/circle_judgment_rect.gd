e816532a8fcd21792a1e03250540c563147f1da0
pyhearts <vs_code@naver.com>
2026-03-31 23:12:52 +0900

    동그라미 판정선 크기 바꾸기 추가

--- window/window-gun/scenes/circle_judgment_rect.gd
+++ window/window-gun/scenes/circle_judgment_rect.gd
@@ -1,12 +1,21 @@
 extends TextureRect
 
-@onready var circle_judgment_rect: TextureRect = $"."
+@onready var circle_judgment: TextureRect = $"."
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	circle_judgment.pivot_offset = circle_judgment.size / 2
+	circle_judgment.size = Vector2(2000,2000)
+	 
 
+
+var a = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	
+	a += delta
+	
+	print(delta)
+	if circle_judgment.size[1] > 1080:
+		circle_judgment.size = Vector2(circle_judgment.size[0]-10, circle_judgment.size[0]-10)
