c7d70708135df0f7bf77aab09666b6d5f98ccdef
pyhearts <vs_code@naver.com>
2026-04-11 21:21:13 +0900

    moving 노드 추가 및 선 연결

--- window/window-gun/scripts/game.gd
+++ window/window-gun/scripts/game.gd
@@ -15,7 +15,10 @@ func _process(delta: float) -> void:
 	
 func _input(event):
 	# ui_accept 액션(Godot 기본 설정: 스페이스바 또는 엔터키)이 눌렸을 때 실행됩니다.
-	if event.is_action_pressed("ui_accept"):
+	if event.is_action_pressed("1"):
 		# 자식 노드가 정상적으로 존재하고, rng_point 함수를 가지고 있는지 확인한 후 실행합니다.
-		if gun_point and gun_point.has_method("rng_point"):
-			gun_point.rng_point()
+		if gun_point and gun_point.has_method("spawn_node"):
+			gun_point.spawn_node("normal")
+	if event.is_action_pressed("2"):
+		if gun_point and gun_point.has_method("spawn_node"):
+			gun_point.spawn_node("moving")
